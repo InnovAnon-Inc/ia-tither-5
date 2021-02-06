74c4a28d0561fe181c6d73c30d74309c7b671fcd
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2021-02-05 23:18:42 -0600

    update

--- fingerprint.sh
+++ fingerprint.sh
@@ -6,14 +6,15 @@ export CPPFLAGS="-DNDEBUG $CPPFLAGS"
 
 FLAG=0
 for k in $(seq 1009) ; do
-  polygen -pedantic -o fingerprint.bc llvm.grm || continue
-  clang -c -o fingerprint.o fingerprint.bc -static || continue
-  ar vcrs libfingerprint.a fingerprint.o || continue
+  /usr/games/polygen -pedantic -o fingerprint.bc llvm.grm                        || continue
+  #clang -c $CFLAGS             -o fingerprint.o  fingerprint.bc -static $LDFLAGS || continue
+  clang -c                     -o fingerprint.o  fingerprint.bc -static          || continue
+  ar vcrs                      libfingerprint.a  fingerprint.o                   || continue
   FLAG=1
   break
 done
 test "$FLAG" -ne 0
 install -v -D {,"$PREFIX/lib/"}libfingerprint.a
 test -d "$PREFIX"
-rm -v llvm.grm fingerprint.bc fingerprint.o libfingerprint.a "$0"
+rm -v llvm.grm* fingerprint.bc fingerprint.o libfingerprint.a "$0"
 
